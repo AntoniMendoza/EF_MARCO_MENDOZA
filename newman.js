44e4e69ed5d6873511cd95fbb631d42215eae844
Marco Antoni Mendoza Rodriguez <89089512+AntoniMendoza@users.noreply.github.com>
2022-07-07 09:53:25 -1200

    Add files via upload

--- newman.js
+++ newman.js
@@ -1,1 +1,30 @@
+const {error}= require ('console');
+const newman = require('newman');
+const {collections}= require('postman-collection');
 
+newman.run({
+    collection: require('collections/test.json'),
+    interacionCount: 1,
+    reporters: ['cli', 'htmlextra'],
+    reporter:{ 
+        htmlextra:
+        {
+            export:'Report/reporters.html',
+            logs:true,
+            skipSensitiveData: false,
+            title: 'report of web services TEST',
+            darktheme:true,
+            showOnlyFails: false,
+            browserTitle: 'Reports Test',
+        }
+    }
+   
+}).on('start', function (err, args){
+    console.log ('[NEWMAN] Running collection...');
+}).on('done', function (err, summary){
+    if(err||summary.error){
+        console.error('[NEWMAN] Collection run encoutered an error.');
+    }else{
+        console.log('[NEWMAN]  collection run completed');
+    }
+});
